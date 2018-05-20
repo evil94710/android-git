8f151395b392d1816a3c4f29de99b88031ab9823
D0549870 <carol408@hotmail.com.tw>
2018-05-20 22:28:18 +0800

    add home UI button

--- app/src/main/java/com/example/user/colormatch/app_home.java
+++ app/src/main/java/com/example/user/colormatch/app_home.java
@@ -2,6 +2,9 @@ package com.example.user.colormatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class app_home extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class app_home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_home);
+        //設定隱藏標題
+        getSupportActionBar().hide();
+        //設定隱藏狀態
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
