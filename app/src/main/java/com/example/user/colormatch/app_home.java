8cb8a0af8a8f175d4d605434ace744fd6452e4e0
D0549870 <carol408@hotmail.com.tw>
2018-05-21 00:00:39 +0800

    link page

--- app/src/main/java/com/example/user/colormatch/app_home.java
+++ app/src/main/java/com/example/user/colormatch/app_home.java
@@ -1,13 +1,16 @@
 package com.example.user.colormatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class app_home extends AppCompatActivity {
-
+    Button logobutton;
+    Button pptbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +19,19 @@ public class app_home extends AppCompatActivity {
         getSupportActionBar().hide();
         //設定隱藏狀態
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        logobutton = (Button)findViewById(R.id.logobutton);
+        logobutton.setOnClickListener(logobutton);
+        pptbutton = (Button)findViewById(R.id.pptbutton);
+        pptbutton.setOnClickListener(pptbutton);
     }
+
+    private View.OnClickListener logo = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent();
+            intent.setClass(app_home.this,Logo.class);
+            startActivity(intent);
+        }
+    };
+
 }
