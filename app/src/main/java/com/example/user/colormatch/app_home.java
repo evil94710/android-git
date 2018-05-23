b73800a8b2e144bab3ad09b782a497fecb670bdb
李香蘭 <evil94710@gmail.com>
2018-05-23 22:02:07 +0800

    修改app_home和Logo

--- app/src/main/java/com/example/user/colormatch/app_home.java
+++ app/src/main/java/com/example/user/colormatch/app_home.java
@@ -1,16 +1,14 @@
 package com.example.user.colormatch;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 public class app_home extends AppCompatActivity {
-    Button logobutton;
-    Button pptbutton;
+    private ImageButton logobutton;
+    private ImageButton pptbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,19 +17,24 @@ public class app_home extends AppCompatActivity {
         getSupportActionBar().hide();
         //設定隱藏狀態
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-        logobutton = (Button)findViewById(R.id.logobutton);
-        logobutton.setOnClickListener(logobutton);
-        pptbutton = (Button)findViewById(R.id.pptbutton);
-        pptbutton.setOnClickListener(pptbutton);
+        logobutton = (ImageButton) findViewById(R.id.logobutton);
+        logobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(app_home.this, Logo.class);
+                startActivity(intent);
+            }
+        });
+        pptbutton = (ImageButton) findViewById(R.id.pptbutton);
+        pptbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(app_home.this, PPT.class);
+                startActivity(intent);
+            }
+        });
     }
-
-    private View.OnClickListener logo = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent();
-            intent.setClass(app_home.this,Logo.class);
-            startActivity(intent);
-        }
-    };
 
 }
