f857633258f570453e8852d5e6527a02f31a6a3b
D0549870 <carol408@hotmail.com.tw>
2018-06-21 14:40:44 +0800

    collect

--- app/src/main/java/com/example/user/colormatch/Collect.java
+++ app/src/main/java/com/example/user/colormatch/Collect.java
@@ -1,11 +1,20 @@
 package com.example.user.colormatch;
 
+import android.content.ClipData;
+import android.content.ClipData.Item;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,10 +24,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Collect extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+
+    private static final int LIST_COLOR = 1;
+    private ColorArrayAdapter adapter = null;
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case LIST_COLOR: {
+                    List<color> colors = (List<color>)msg.obj;
+                    refreshColorList(colors);
+                    break;
+                }
+            }
+        }
+    };
+
+
+    private void refreshColorList(List<color> colors) {
+        adapter.clear();
+        adapter.addAll(colors);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,6 +91,114 @@ public class Collect extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        ListView lv = (ListView)findViewById(R.id.lv_collect);
+
+        adapter = new ColorArrayAdapter(this, new ArrayList<color>());
+        lv.setAdapter(adapter);
+
+        getColorFromFirebase();
+
+
+    }
+
+    class FirebaseThread extends Thread {
+
+        private DataSnapshot dataSnapshot;
+
+        public FirebaseThread(DataSnapshot dataSnapshot) {
+
+            this.dataSnapshot = dataSnapshot;
+        }
+
+        @Override
+        public void run() {
+            List<color> lsColor = new ArrayList<>();
+            for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                DataSnapshot dsName = ds.child("Name");
+                DataSnapshot dsCode1 = ds.child("colorcode1");
+                DataSnapshot dsCode2 = ds.child("colorcode2");
+                DataSnapshot dsCode3 = ds.child("colorcode3");
+                DataSnapshot dsCode4 = ds.child("colorcode4");
+                DataSnapshot dsCode5 = ds.child("colorcode5");
+
+                String Name = (String)dsName.getValue();
+                String code1 = (String)dsCode1.getValue();
+                String code2 = (String)dsCode2.getValue();
+                String code3 = (String)dsCode3.getValue();
+                String code4 = (String)dsCode4.getValue();
+                String code5 = (String)dsCode5.getValue();
+
+
+
+                color aColor = new color();
+                aColor.setName(Name);
+                aColor.setCode1(code1);
+                aColor.setCode2(code1);
+                aColor.setCode3(code1);
+                aColor.setCode4(code1);
+                aColor.setCode5(code1);
+                lsColor.add(aColor);
+                //Log.v("hotel", Name + ";" + address);
+
+            }
+            Message msg = new Message();
+            msg.what = LIST_COLOR;
+            msg.obj = lsColor;
+            handler.sendMessage(msg);
+        }
+    }
+
+    private void getColorFromFirebase() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                new FirebaseThread(dataSnapshot).start();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //Log.v("color", databaseError.getMessage());
+            }
+        });
+    }
+
+
+    class ColorArrayAdapter extends ArrayAdapter<color> {
+        Context context;
+
+        public ColorArrayAdapter(Context context, List<color> items) {
+            super(context, 0, items);
+            this.context = context;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            LinearLayout itemlayout = null;
+            if (convertView == null) {
+                itemlayout = (LinearLayout) inflater.inflate(R.layout.listitem, null);
+            } else {
+                itemlayout = (LinearLayout) convertView;
+            }
+            color item = (color) getItem(position);
+            TextView tvName = (TextView) itemlayout.findViewById(R.id.tv_name);
+            tvName.setText(item.getName());
+            TextView tvCode1 = (TextView) itemlayout.findViewById(R.id.tv_code1);
+            tvCode1.setText(item.getCode1());
+            TextView tvCode2 = (TextView) itemlayout.findViewById(R.id.tv_code2);
+            tvCode2.setText(item.getCode2());
+            TextView tvCode3 = (TextView) itemlayout.findViewById(R.id.tv_code3);
+            tvCode3.setText(item.getCode3());
+            TextView tvCode4 = (TextView) itemlayout.findViewById(R.id.tv_code4);
+            tvCode4.setText(item.getCode4());
+            TextView tvCode5 = (TextView) itemlayout.findViewById(R.id.tv_code5);
+            tvCode5.setText(item.getCode5());
+            return itemlayout;
+        }
     }
 
     @Override
@@ -111,16 +267,6 @@ public class Collect extends AppCompatActivity
             };
             ad.setPositiveButton("OK",listener);
             ad.show();
-        } else if (id == R.id.logo){
-            Intent intent = new Intent();
-            intent.setClass(Collect.this, Logo.class);
-            startActivity(intent);
-            Collect.this.finish();
-        } else if (id == R.id.ppt){
-            Intent intent = new Intent();
-            intent.setClass(Collect.this, PPT.class);
-            startActivity(intent);
-            Collect.this.finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
---
+++ app/src/main/java/com/example/user/colormatch/color.java
@@ -0,0 +1,59 @@
+package com.example.user.colormatch;
+
+public class color {
+
+    private String name;
+    private String code1;
+    private String code2;
+    private String code3;
+    private String code4;
+    private String code5;
+
+    public String getCode5() {
+        return code5;
+    }
+
+    public void setCode5(String code5) {
+        this.code5 = code5;
+    }
+
+    public String getCode4() {
+        return code4;
+    }
+
+    public void setCode4(String code4) {
+        this.code4 = code4;
+    }
+
+    public String getCode3() {
+        return code3;
+    }
+
+    public void setCode3(String code3) {
+        this.code3 = code3;
+    }
+
+    public String getCode2() {
+        return code2;
+    }
+
+    public void setCode2(String code2) {
+        this.code2 = code2;
+    }
+
+    public String getCode1() {
+        return code1;
+    }
+
+    public void setCode1(String code1) {
+        this.code1 = code1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
