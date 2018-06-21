056166247f5df727102845ea4e9f801efae7ed5b
李香蘭 <evil94710@gmail.com>
2018-06-21 11:55:52 +0800

    ADD 新增資料(未完成)

--- app/src/main/java/com/example/user/colormatch/add.java
+++ app/src/main/java/com/example/user/colormatch/add.java
@@ -19,6 +19,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
 
 public class add extends AppCompatActivity
@@ -29,6 +34,26 @@ public class add extends AppCompatActivity
     private ImageButton button3;
     private ImageButton button4;
     private ImageButton button5;
+    private TextView R1;
+    private TextView R2;
+    private TextView R3;
+    private TextView R4;
+    private TextView R5;
+    private TextView G1;
+    private TextView G2;
+    private TextView G3;
+    private TextView G4;
+    private TextView G5;
+    private TextView B1;
+    private TextView B2;
+    private TextView B3;
+    private TextView B4;
+    private TextView B5;
+    private TextView C1;
+    private TextView C2;
+    private TextView C3;
+    private TextView C4;
+    private TextView C5;
     private Button button;
     private String colorchange;
 
@@ -38,7 +63,6 @@ public class add extends AppCompatActivity
         setContentView(R.layout.activity_add);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -57,14 +81,14 @@ public class add extends AppCompatActivity
                 cp.setCallback(new ColorPickerCallback() {
                     @Override
                     public void onColorChosen(int color) {
-                        TextView red = findViewById(R.id.r1);
-                        TextView green = findViewById(R.id.g1);
-                        TextView blue = findViewById(R.id.b1);
-                        TextView colorcode = findViewById(R.id.c1);
-                        red.setText("R : " + Integer.toString(Color.red(color)));
-                        blue.setText("G : " + Integer.toString(Color.blue(color)));
-                        green.setText("B : " + Integer.toString(Color.green(color)));
-                        colorcode.setText(String.format("#%06X", (0xFFFFFF & color)));
+                        R1 = findViewById(R.id.r1);
+                        G1 = findViewById(R.id.g1);
+                        B1 = findViewById(R.id.b1);
+                        C1 = findViewById(R.id.c1);
+                        R1.setText("R : " + Integer.toString(Color.red(color)));
+                        G1.setText("B : " + Integer.toString(Color.green(color)));
+                        B1.setText("G : " + Integer.toString(Color.blue(color)));
+                        C1.setText(String.format("#%06X", (0xFFFFFF & color)));
 
                         colorchange = String.format("#%06X", (0xFFFFFF & color));
                         ImageButton ib1 = findViewById(R.id.ib1);
@@ -82,14 +106,14 @@ public class add extends AppCompatActivity
                 cp.setCallback(new ColorPickerCallback() {
                     @Override
                     public void onColorChosen(int color) {
-                        TextView red = findViewById(R.id.r2);
-                        TextView green = findViewById(R.id.g2);
-                        TextView blue = findViewById(R.id.b2);
-                        TextView colorcode = findViewById(R.id.c2);
-                        red.setText("R : " + Integer.toString(Color.red(color)));
-                        blue.setText("G : " + Integer.toString(Color.blue(color)));
-                        green.setText("B : " + Integer.toString(Color.green(color)));
-                        colorcode.setText(String.format("#%06X", (0xFFFFFF & color)));
+                        R2 = findViewById(R.id.r2);
+                        G2 = findViewById(R.id.g2);
+                        B2 = findViewById(R.id.b2);
+                        C2 = findViewById(R.id.c2);
+                        R2.setText("R : " + Integer.toString(Color.red(color)));
+                        G2.setText("B : " + Integer.toString(Color.green(color)));
+                        B2.setText("G : " + Integer.toString(Color.blue(color)));
+                        C2.setText(String.format("#%06X", (0xFFFFFF & color)));
 
                         colorchange = String.format("#%06X", (0xFFFFFF & color));
                         ImageButton ib2 = findViewById(R.id.ib2);
@@ -107,14 +131,14 @@ public class add extends AppCompatActivity
                 cp.setCallback(new ColorPickerCallback() {
                     @Override
                     public void onColorChosen(int color) {
-                        TextView red = findViewById(R.id.r3);
-                        TextView green = findViewById(R.id.g3);
-                        TextView blue = findViewById(R.id.b3);
-                        TextView colorcode = findViewById(R.id.c3);
-                        red.setText("R : " + Integer.toString(Color.red(color)));
-                        blue.setText("G : " + Integer.toString(Color.blue(color)));
-                        green.setText("B : " + Integer.toString(Color.green(color)));
-                        colorcode.setText(String.format("#%06X", (0xFFFFFF & color)));
+                        R3 = findViewById(R.id.r3);
+                        G3 = findViewById(R.id.g3);
+                        B3 = findViewById(R.id.b3);
+                        C3 = findViewById(R.id.c3);
+                        R3.setText("R : " + Integer.toString(Color.red(color)));
+                        G3.setText("B : " + Integer.toString(Color.green(color)));
+                        B3.setText("G : " + Integer.toString(Color.blue(color)));
+                        C3.setText(String.format("#%06X", (0xFFFFFF & color)));
 
                         colorchange = String.format("#%06X", (0xFFFFFF & color));
                         ImageButton ib3 = findViewById(R.id.ib3);
@@ -132,14 +156,14 @@ public class add extends AppCompatActivity
                 cp.setCallback(new ColorPickerCallback() {
                     @Override
                     public void onColorChosen(int color) {
-                        TextView red = findViewById(R.id.r4);
-                        TextView green = findViewById(R.id.g4);
-                        TextView blue = findViewById(R.id.b4);
-                        TextView colorcode = findViewById(R.id.c4);
-                        red.setText("R : " + Integer.toString(Color.red(color)));
-                        blue.setText("G : " + Integer.toString(Color.blue(color)));
-                        green.setText("B : " + Integer.toString(Color.green(color)));
-                        colorcode.setText(String.format("#%06X", (0xFFFFFF & color)));
+                        R4 = findViewById(R.id.r4);
+                        G4 = findViewById(R.id.g4);
+                        B4 = findViewById(R.id.b4);
+                        C4 = findViewById(R.id.c4);
+                        R4.setText("R : " + Integer.toString(Color.red(color)));
+                        G4.setText("B : " + Integer.toString(Color.green(color)));
+                        B4.setText("G : " + Integer.toString(Color.blue(color)));
+                        C4.setText(String.format("#%06X", (0xFFFFFF & color)));
 
                         colorchange = String.format("#%06X", (0xFFFFFF & color));
                         ImageButton ib4 = findViewById(R.id.ib4);
@@ -157,14 +181,14 @@ public class add extends AppCompatActivity
                 cp.setCallback(new ColorPickerCallback() {
                     @Override
                     public void onColorChosen(int color) {
-                        TextView red = findViewById(R.id.r5);
-                        TextView green = findViewById(R.id.g5);
-                        TextView blue = findViewById(R.id.b5);
-                        TextView colorcode = findViewById(R.id.c5);
-                        red.setText("R : " + Integer.toString(Color.red(color)));
-                        blue.setText("G : " + Integer.toString(Color.blue(color)));
-                        green.setText("B : " + Integer.toString(Color.green(color)));
-                        colorcode.setText(String.format("#%06X", (0xFFFFFF & color)));
+                        R5 = findViewById(R.id.r5);
+                        G5 = findViewById(R.id.g5);
+                        B5 = findViewById(R.id.b5);
+                        C5 = findViewById(R.id.c5);
+                        R5.setText("R : " + Integer.toString(Color.red(color)));
+                        G5.setText("B : " + Integer.toString(Color.green(color)));
+                        B5.setText("G : " + Integer.toString(Color.blue(color)));
+                        C5.setText(String.format("#%06X", (0xFFFFFF & color)));
 
                         colorchange = String.format("#%06X", (0xFFFFFF & color));
                         ImageButton ib5 = findViewById(R.id.ib5);
@@ -173,13 +197,64 @@ public class add extends AppCompatActivity
                 });
             }
         });
-        EditText name = findViewById(R.id.name);
-        String Name = name.getText().toString();
+        final EditText name = findViewById(R.id.name);
+        final String Name = name.getText().toString();
         button = findViewById(R.id.add);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                final DatabaseReference myref = database.getReference("");
+                myref.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        /*for(dataSnapshot : dataSnapshot.getChildren()){
+                            DataSnapshot dsName = dataSnapshot.child("Name");
+                            DataSnapshot dsR1 = dataSnapshot.child("R1");
+                            DataSnapshot dsR2 = dataSnapshot.child("R2");
+                            DataSnapshot dsR3 = dataSnapshot.child("R3");
+                            DataSnapshot dsR4 = dataSnapshot.child("R4");
+                            DataSnapshot dsR5 = dataSnapshot.child("R5");
+                            DataSnapshot dsG1 = dataSnapshot.child("G1");
+                            DataSnapshot dsG2 = dataSnapshot.child("G2");
+                            DataSnapshot dsG3 = dataSnapshot.child("G3");
+                            DataSnapshot dsG4 = dataSnapshot.child("G4");
+                            DataSnapshot dsG5 = dataSnapshot.child("G5");
+                            DataSnapshot dsB1 = dataSnapshot.child("B1");
+                            DataSnapshot dsB2 = dataSnapshot.child("B2");
+                            DataSnapshot dsB3 = dataSnapshot.child("B3");
+                            DataSnapshot dsB4 = dataSnapshot.child("B4");
+                            DataSnapshot dsB5 = dataSnapshot.child("B5");
+
+                        }*/
+                        String r1 = R1.getText().toString();
+                        String r2 = R2.getText().toString();
+                        String r3 = R3.getText().toString();
+                        String r4 = R4.getText().toString();
+                        String r5 = R5.getText().toString();
+                        String g1 = G1.getText().toString();
+                        String g2 = G2.getText().toString();
+                        String g3 = G3.getText().toString();
+                        String g4 = G4.getText().toString();
+                        String g5 = G5.getText().toString();
+                        String b1 = B1.getText().toString();
+                        String b2 = B2.getText().toString();
+                        String b3 = B3.getText().toString();
+                        String b4 = B4.getText().toString();
+                        String b5 = B5.getText().toString();
+                        long n = dataSnapshot.getChildrenCount();
+                        String num = String.valueOf(n);
+                        /*DatabaseReference ref = myref.child(num);
+                        colorData colorData = new colorData(Name,r1,r2,r3,r4,r5,g1,g2,g3,g4,g5,b1,b2,b3,b4,b5);
+                        ref.setValue(colorData);*/
+                        //Log.d("total",String.valueOf(n));
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
             }
         });
     }
---
+++ app/src/main/java/com/example/user/colormatch/colorData.java
@@ -0,0 +1,170 @@
+package com.example.user.colormatch;
+
+
+public class colorData {
+    private String name;
+    private String R1;
+    private String R2;
+    private String R3;
+    private String R4;
+    private String R5;
+    private String G1;
+    private String G2;
+    private String G3;
+    private String G4;
+    private String G5;
+    private String B1;
+    private String B2;
+    private String B3;
+    private String B4;
+    private String B5;
+
+    public colorData(String name, String R1, String R2, String R3, String R4, String R5,
+                     String G1, String G2, String G3, String G4, String G5, String B1,
+                     String B2, String B3, String B4, String B5) {
+        this.name = name;
+        this.R1 = R1;
+        this.R2 = R2;
+        this.R3 = R3;
+        this.R4 = R4;
+        this.R5 = R5;
+        this.G1 = G1;
+        this.G2 = G2;
+        this.G3 = G3;
+        this.G4 = G4;
+        this.G5 = G5;
+        this.B1 = B1;
+        this.B2 = B2;
+        this.B3 = B3;
+        this.B4 = B4;
+        this.B5 = B5;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getR1() {
+        return R1;
+    }
+
+    public void setR1(String r1) {
+        R1 = r1;
+    }
+
+    public String getR2() {
+        return R2;
+    }
+
+    public void setR2(String r2) {
+        R2 = r2;
+    }
+
+    public String getR3() {
+        return R3;
+    }
+
+    public void setR3(String r3) {
+        R3 = r3;
+    }
+
+    public String getR4() {
+        return R4;
+    }
+
+    public void setR4(String r4) {
+        R4 = r4;
+    }
+
+    public String getR5() {
+        return R5;
+    }
+
+    public void setR5(String r5) {
+        R5 = r5;
+    }
+
+    public String getG1() {
+        return G1;
+    }
+
+    public void setG1(String g1) {
+        G1 = g1;
+    }
+
+    public String getG2() {
+        return G2;
+    }
+
+    public void setG2(String g2) {
+        G2 = g2;
+    }
+
+    public String getG3() {
+        return G3;
+    }
+
+    public void setG3(String g3) {
+        G3 = g3;
+    }
+
+    public String getG4() {
+        return G4;
+    }
+
+    public void setG4(String g4) {
+        G4 = g4;
+    }
+
+    public String getG5() {
+        return G5;
+    }
+
+    public void setG5(String g5) {
+        G5 = g5;
+    }
+
+    public String getB1() {
+        return B1;
+    }
+
+    public void setB1(String b1) {
+        B1 = b1;
+    }
+
+    public String getB2() {
+        return B2;
+    }
+
+    public void setB2(String b2) {
+        B2 = b2;
+    }
+
+    public String getB3() {
+        return B3;
+    }
+
+    public void setB3(String b3) {
+        B3 = b3;
+    }
+
+    public String getB4() {
+        return B4;
+    }
+
+    public void setB4(String b4) {
+        B4 = b4;
+    }
+
+    public String getB5() {
+        return B5;
+    }
+
+    public void setB5(String b5) {
+        B5 = b5;
+    }
+}
